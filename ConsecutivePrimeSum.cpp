9d34035ce02a45dc29a4e20b365d35adbc9be7c2
mgruben <GrubenM@gmail.com>
2016-12-08 10:01:49 -0600

    Implement sieve of Eratosthenes

--- ConsecutivePrimeSum.cpp
+++ ConsecutivePrimeSum.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 
 using namespace std;
 
@@ -23,5 +24,42 @@ bool isPrime(int n) {
  * the current sum.
  */
 vector<int> firstNPrimes(int N) {
+    vector<int> v;
+    return v;
+}
+
+/**
+ * Return all prime numbers not greater than some integer n.
+ * 
+ * https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes#Pseudocode
+ */
+vector<int> sieve(int n) {
+    vector<int> v;
+    if (n < 2) return v;
     
+    bool p[n];
+    p[0] = p[1] = false;
+    for (int i = 2; i < n; i++) p[i] = true;
+    
+    for (int i = 2; i * i < n; i++) {
+        if (p[i]) {
+            v.push_back(i);
+            for (int j = i * i; j < n; j += i) {
+                p[j] = false;
+            }
+        }
+    }
+    
+    for (int i = (int) sqrt(n) + 1; i < n; i++) if (p[i]) v.push_back(i);
+    return v;
+}
+
+void test() {
+    for (int i: sieve(1000)) cout << i << " ";
+    cout << endl;
+}
+
+int main() {
+    test();
+    return 0;
 }
